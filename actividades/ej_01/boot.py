cce018ea3a10b53eebafd489252a55f049189fda
pablooosores <101607991+pablooosores@users.noreply.github.com>
2025-04-25 07:48:04 -0300

    Update boot.py

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,22 @@
-# Configuracion inicial
+from microdot import Microdot
+from microdot import send_file
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return 'Hello, world!'
+
+@app.get('index.html')
+async def index(request):
+    return send_file('/index.html')
+
+@app.get('styles.css')
+async def index(request):
+    return send_file('/styles/base.css')
+
+@app.get('scripts.js')
+async def index(request):
+    return send_file('/scripts/base.js')
+
+app.run()
